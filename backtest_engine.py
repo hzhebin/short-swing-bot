8e5fdc46bc9aff654f2eaba518c7d56a00e71cbb
hzhebin <50128350+hzhebin@users.noreply.github.com>
2025-05-27 16:14:27 +0800

    Add files via upload

--- backtest_engine.py
+++ backtest_engine.py
@@ -1,8 +1,3 @@
 # backtest_engine.py
-
-class BacktestEngine:
-    def __init__(self):
-        pass
-
-    def run_backtest(self):
-        print('Running backtest...')
+def run_backtest():
+    print("Running backtest...")
--- data_feed.py
+++ data_feed.py
@@ -1,5 +1,3 @@
 # data_feed.py
-
-def fetch_data(symbol, interval):
-    print(f'Fetching {interval} data for {symbol}')
-    return []
+def fetch_data():
+    print("Fetching data...")
